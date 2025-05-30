50ca7ec65fceaae6b39f34e46d15e7bacdf8ee2c
dimakatsobopape <Dimakzela@gmail.com>
2025-05-30 15:07:19 +0200

    config springboot docker

--- rentals-web/src/app/app.component.ts
+++ rentals-web/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from './navbar/navbar.component';
-import {DatePipe} from '@angular/common';
 import {FooterComponent} from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, DatePipe, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- rentals-web/src/app/home/home.component.ts
+++ rentals-web/src/app/home/home.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
-import {MatFormField} from "@angular/material/form-field";
-import {MatIcon} from "@angular/material/icon";
-import {MatInput} from "@angular/material/input";
-import {MatIconButton} from "@angular/material/button";
 import {SearchBarComponent} from "../search-bar/search-bar.component";
 import {PropertyListComponent} from "../property-list/property-list.component";
 
 @Component({
   selector: 'app-home',
   imports: [
-    MatFormField,
-    MatIcon,
-    MatInput,
-    MatIconButton,
     SearchBarComponent,
     PropertyListComponent
   ],
